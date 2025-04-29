047200f9e5c5443a54362fac29d68bc7c6397f73
Joseph Oforkansi <143688866+josephoforkansi@users.noreply.github.com>
2025-04-28 21:19:36 -0400

    Update SLL.py

--- analyze/SLL.py
+++ analyze/SLL.py
@@ -1,99 +1,147 @@
-"""Basic Singly Linked List Queue implementation."""
-
-from typing import Any, Optional
-from dataclasses import dataclass
+""" "Implementation of a singly linked list."""
 
 
-@dataclass
 class Node:
-    """Node class for singly linked list."""
-    value: Any
-    next: Optional["Node"] = None
+    """Represents a node in the singly linked list."""
+
+    def __init__(self, data):
+        self.data = data
+        self.link = None
 
 
-class BasicSLLQueue:
-    """Optimized Singly Linked List Queue implementation with tail pointer."""
+class SinglyLinkedList:
+    """Represents a singly linked list."""
 
     def __init__(self):
-        """Initialize an empty queue with head and tail pointers."""
-        self.head: Optional[Node] = None
-        self.tail: Optional[Node] = None
-        self.size: int = 0
+        self._head = None
+        self._tail = None
+        self._length = 0
 
-    def is_empty(self) -> bool:
-        """Check if the queue is empty."""
-        return self.size == 0
+    def is_empty(self):
+        """Checks if the list is empty."""
+        return self._head is None
 
-    def enqueue(self, value: Any) -> None:
-        """Add an element to the end of the queue. O(1) operation using tail pointer."""
-        new_node = Node(value)
+    def addfirst(self, data):
+        """Adds a new node with the given data to the beginning of the list."""
+        new_node = Node(data)
         if self.is_empty():
-            self.head = new_node
+            self._tail = new_node
+        new_node.link = self._head
+        self._head = new_node
+        self._length += 1
+
+        assert self._head is not None, "Head should not be None after adding"
+        assert self._length > 0, "Length should be greater than 0 after adding"
+
+    def addlast(self, data):
+        """Adds a new node with the given data to the end of the list."""
+        new_node = Node(data)
+        if self.is_empty():
+            self._head = new_node
+            self._tail = new_node
         else:
-            self.tail.next = new_node  # Directly append at tail
-        self.tail = new_node  # Update tail pointer
-        self.size += 1
+            self._tail.link = new_node
+            self._tail = new_node
+        self._length += 1
 
-    def dequeue(self) -> Any:
-        """Remove and return the first element from the queue. O(1) operation."""
+    def removefirst(self):
+        item = self._head.data
+        self._head = self._head.link
+        if self._head is None:
+            self._tail = None
+        self._length -= 1
+        return item
+
+    def removelast(self):
+        """Removes and returns the data from the last node of the list."""
         if self.is_empty():
-            raise IndexError("Queue is empty")
-        value = self.head.value
-        self.head = self.head.next
-        if self.head is None:  # If queue is now empty, reset tail
-            self.tail = None
-        self.size -= 1
-        return value
+            return None
+        if self._head == self._tail:
+            data = self._head.data
+            self._head = None
+            self._tail = None
+            self._length = 0
+            return data
+        current = self._head
+        while current.link != self._tail:
+            current = current.link
+        data = self._tail.data
+        current.link = None
+        self._tail = current
+        self._length -= 1
+        return data
 
-    def peek(self) -> Any:
-        """Return the first element without removing it. O(1) operation."""
-        if self.is_empty():
-            raise IndexError("Queue is empty")
-        return self.head.value
+    def __len__(self):
+        """Returns the length of the list."""
+        return self._length
 
-    def removelast(self) -> Any:
-        """Remove and return the last element from the queue. O(n) operation."""
-        if self.is_empty():
-            raise IndexError("Queue is empty")
-        
-        if self.head == self.tail:  # Only one element
-            value = self.head.value
-            self.head = None
-            self.tail = None
-        else:
-            # Traverse to find second-to-last node
-            current = self.head
-            while current.next != self.tail:
-                current = current.next
-            value = self.tail.value
-            current.next = None
-            self.tail = current
-        
-        self.size -= 1
-        return value
+    def __add__(self, other):
+        """Concatenates two singly linked lists, returning a new list."""
+        new_list = SinglyLinkedList()
+        if self._head:
+            new_list._head = Node(self._head.data)
+            current = self._head.link
+            new_list_current = new_list._head
+            while current:
+                new_node = Node(current.data)
+                new_list_current.link = new_node
+                new_list_current = new_node
+                current = current.link
+            new_list._tail = new_list_current
+            new_list._length = self._length
 
-    def __add__(self, other: "BasicSLLQueue") -> "BasicSLLQueue":
-        """Concatenate two queues. O(1) operation using tail pointer."""
-        if self.is_empty():
-            return other
-        if other.is_empty():
-            return self
+            if other._head:
+                new_list._tail.link = Node(other._head.data)
+                current = other._head.link
+                new_list_current = new_list._tail.link
+                while current:
+                    new_node = Node(current.data)
+                    new_list_current.link = new_node
+                    new_list_current = new_node
+                    current = current.link
+                new_list._tail = new_list_current
+                new_list._length += other._length
+        elif other._head:
+            new_list = other.__copy__()
+        return new_list
 
-        result = BasicSLLQueue()
-        result.head = self.head
-        result.tail = other.tail
-        result.size = self.size + other.size
+    def __copy__(self):
+        """Creates a shallow copy of the singly linked list."""
+        new_list = SinglyLinkedList()
+        if self._head:
+            new_list._head = Node(self._head.data)
+            current = self._head.link
+            new_list_current = new_list._head
+            while current:
+                new_node = Node(current.data)
+                new_list_current.link = new_node
+                new_list_current = new_node
+                current = current.link
+            new_list._tail = new_list_current
+            new_list._length = self._length
+        return new_list
 
-        self.tail.next = other.head  # Connect self to other
-        return result
-
-    def __iadd__(self, other: "BasicSLLQueue") -> "BasicSLLQueue":
-        """Concatenate another queue to this queue. O(1) operation."""
-        if self.is_empty():
-            self.head = other.head
-            self.tail = other.tail
-        elif not other.is_empty():
-            self.tail.next = other.head
-            self.tail = other.tail
-        self.size += other.size
+    def __iadd__(self, other):
+        """In-place concatenation of two singly linked lists."""
+        if other._head is not None:
+            if self._head is None:
+                self._head = other._head
+                self._tail = other._tail
+            else:
+                self._tail.link = other._head
+                self._tail = other._tail
+            self._length += other._length
+            # Empty the other list manually
+            other._head = None
+            other._tail = None
+            other._length = 0
         return self
+
+    def __str__(self):
+        """Returns a string representation of the list."""
+        elements = []
+        current = self._head
+        while current:
+            elements.append(str(current.data))
+            current = current.link
+        return " -> ".join(elements)
